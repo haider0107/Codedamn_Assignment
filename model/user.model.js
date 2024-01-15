8c2c666e004214302a0ff6a27867cf61fee34371
haider0107 <kushal.git01@gmail.com>
2024-01-15 13:34:42 +0530

    User model created

--- model/user.model.js
+++ model/user.model.js
@@ -1,2 +1,10 @@
 import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
